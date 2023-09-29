6d0e7eb07677679fbe903ca225f41c440cc8e21b
Julian Florez <forezjuli08@hotmail.com>
2023-09-29 17:45:37 -0500

    add - code for chapter 1

---
+++ ch1/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hello, world!!")
+
+	// use go run hello.go to run this program as a scripting language
+	// use go build hello.go to create an executable hello.exe in the same directory
+	// alternatively, we can use go build -o hello_world hello.go to change the executables name (we can also change the output location)
+}
